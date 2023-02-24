29d727a2533a98521337f5128b47a3cb9b86a7ed
yuI4140 <102521443+yuI4140@users.noreply.github.com>
2023-02-24 21:06:45 +0100

    reinit

--- core.h
+++ core.h
@@ -1,8 +1,8 @@
-#include<iostream>
-#include<cmath>
-#include<string>
-#include<windows.h>
-#include<chrono>
-#include<thread>
-#include<functional>
-#include<cstdio>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<assert.h>
+#include<math.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdbool.h>
